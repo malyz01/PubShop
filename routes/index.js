7a5cac9423689b28fcfdbdc63956b0d23db77012
Malyz <mattuy001@gmail.com>
2019-02-12 16:10:08 +1300

    typo

--- routes/index.js
+++ routes/index.js
@@ -106,9 +106,9 @@ router.post("/forgot", (req, res, next) => {
           from: "autoresponse1010@gmail.com",
           subject: "PubShop Password Reset",
           text: `You are receiving this email because you have requested to reset your password. 
-          If you did not request for this, kindly ignore this email.
-          Please click on the following link or paste this into your browser to complete the process:
-          https://experimentalpubshop.herokuapp.com/reset/${token}`
+If you did not request for this, kindly ignore this email.
+Please click on the following link or paste this into your browser to complete the process:
+https://experimentalpubshop.herokuapp.com/reset/${token}`
         };
         smtpTransport.sendMail(mailOptions, err => {
           req.flash(
@@ -157,7 +157,7 @@ router.post("/reset/:token", (req, res) => {
             if (!foundUser) {
               req.flash(
                 "error",
-                "Password reset token if invalid or has expired."
+                "Password reset token is invalid or has expired."
               );
               return res.redirect("back");
             }
